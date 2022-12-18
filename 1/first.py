79a7eaaad8ae890c77bb8936a4b54187028ac4a5
jayson7 <lexxiijoo70@gmail.com>
2022-12-18 13:34:16 +0100

    sklearn added

--- 1/first.py
+++ 1/first.py
@@ -24,6 +24,7 @@ x = df.iloc[:, [3,4]].values
 y = df.iloc[:, [3,4]].values[0:20]
 print(x)
 
+#  instigating Elbow method to get numbers of all optimal clusters
 wcss = []
 
 for i in range(1,11):
@@ -36,3 +37,20 @@ plt.title('The elbow method')
 plt.xlabel('No. of clusters')
 plt.ylabel('WCSS values')
 plt.show()
+
+# Using unsupervised learning algorithm to train model i.e. KMEANS
+
+k_means_model = KMeans(n_clusters=5, init = 'k-means++', random_state=0)
+y_kmeans = k_means_model.fit_predict(x)
+plt.scatter(x[y_kmeans == 0,0], x[y_kmeans==0,1], s = 80, c = 'red', label = 'Customer 1')
+plt.scatter(x[y_kmeans == 1,0], x[y_kmeans==1,1], s = 80, c = 'blue', label = 'Customer 2')
+plt.scatter(x[y_kmeans == 2,0], x[y_kmeans==2,1], s = 80, c = 'green', label = 'Customer 3')
+plt.scatter(x[y_kmeans == 3,0], x[y_kmeans==3,1], s = 80, c = 'yellow', label = 'Customer 4')
+plt.scatter(x[y_kmeans == 4,0], x[y_kmeans==4,1], s = 80, c = 'purple', label = 'Customer 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1],s =100, c= 'magenta', label = 'Centroids')
+plt.title("Cluster of Customers")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1 - 100)")
+plt.legend()
+plt.show()
+
